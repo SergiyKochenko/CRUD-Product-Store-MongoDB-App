abd06a2f329effa344af57c41935c98bf6af8869
Sergiy <sergiykochenko@gmail.com>
2025-04-12 19:39:35 +0100

    Chakra UI Setup

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
@@ -14,7 +15,7 @@ app.use('/api/products', productRoutes);
 const mongoUri = process.env.MONGO_URI;
 console.log(mongoUri);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server started at http://localhost:5000');
+    console.log('Server started at http://localhost:' + PORT);
 });
